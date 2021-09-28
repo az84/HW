fc3ce54eaa2de344bcb76acce3ba8c86b7405007
azaidi <zaidialina1@gmail.com>
2021-09-28 06:44:03 -0400

    all tests passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,19 +1,20 @@
 const Engineer = require("../lib/Engineer");
 
-describe("Setting GitHUb account via constructor", () => {
+test("Setting GitHUb account via constructor", () => {
   const runTest = "GitHubUser";
   const eng = new Engineer("hello", 1, "test@test.com", runTest);
   expect(eng.github).toEqual(runTest);
 });
 
-describe("getRole() return \"Engineer\"", () => {
+test("getRole() return \"Engineer\"", () => {
   const runTest = "Engineer";
   const eng = new Engineer("hello", 1, "test@test.com", "GitHubUser");
   expect(eng.getRole()).toEqual(runTest);
 });
 
-describe("GitHub username via getGithub()", () => {
+test("GitHub username via getGithub()", () => {
   const runTest = "GitHubUser";
   const eng = new Engineer("hello", 1, "test@test.com", runTest);
   expect(eng.getGithub()).toEqual(runTest);
 });
+
